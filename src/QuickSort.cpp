bf75e57da02e82b5444bcf74efe19e9af1f6ac20
nhipham <nhivpham12@gmailcom>
2025-04-20 16:30:52 -0400

    add clarifying comments and fix a couple variables

--- src/QuickSort.cpp
+++ src/QuickSort.cpp
@@ -3,6 +3,7 @@
 
 namespace Sorter {
 
+    // compares two days in chronological order
     bool QuickSort::comesBefore(const DayList::Day* a, const DayList::Day* b) {
         if (a->year  != b->year)  return a->year  < b->year;
         if (a->month != b->month) return a->month < b->month;
@@ -25,6 +26,7 @@ namespace Sorter {
         return i+1;
     }
 
+    // recursion on vector
     void QuickSort::quickSortVec(std::vector<DayList::Day*>& arr, int low, int high) {
         if (low < high) {
             int pi = partition(arr, low, high);
@@ -33,28 +35,29 @@ namespace Sorter {
         }
     }
 
-    void QuickSort::sortDays(DayList::Day** headRef, DayList::Day** tailRef) {
+    void QuickSort::sortDays(DayList::Day** headPointer, DayList::Day** tailPointer) {
+        // puts nodes into an array
         std::vector<DayList::Day*> arr;
 
-        for (DayList::Day* cur = *headRef; cur; cur = cur->next) {
+        for (DayList::Day* cur = *headPointer; cur; cur = cur->next) {
             arr.push_back(cur);
         }
 
         if (arr.empty()) return;
 
-        // sort
+        // sorts array
         quickSortVec(arr, 0, static_cast<int>(arr.size()) - 1);
 
-        // rebuild pointers
+        // remake link list in sorted order
         for (size_t i = 0; i + 1 < arr.size(); ++i) {
             arr[i]->next = arr[i+1];
         }
 
         arr.back()->next = nullptr;
 
-        // reset head & tail
-        *headRef = arr.front();
-        *tailRef = arr.back();
+        // update head & tail in original list
+        *headPointer = arr.front();
+        *tailPointer = arr.back();
     }
 
 }
--- src/QuickSort.h
+++ src/QuickSort.h
@@ -8,14 +8,18 @@ namespace Sorter {
     class QuickSort {
     public:
         // sorts the link list where the head or tail pointers are passed into
-        static void sortDays(DayList::Day** headRef, DayList::Day** tailRef);
+        // headPointer points to the earliest day
+        // tailPointer points to the last day
+        static void sortDays(DayList::Day** headPointer, DayList::Day** tailPointer);
 
     private:
-        // pivot & partition helpers
+        // compares two Day pointers (yy, mm, dd)
         static bool comesBefore(const DayList::Day* a, const DayList::Day* b);
 
+        // moves elements greater than pivot to left of index
         static int partition(std::vector<DayList::Day*>& arr, int low, int high);
 
+        // quick sort recursion
         static void quickSortVec(std::vector<DayList::Day*>& arr, int low, int high);
     };
 }
